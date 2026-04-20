c2d0729aa34f6c8b43f3457babbb3d823794d9c4
Iago Vitor <iagovls@gmail.com>
2026-04-20 12:30:45 -0300

    delete desnecessary items

--- extractEvents.js
+++
@@ -1,7 +0,0 @@
-function main(arg1) {
-  return arg1 
-}
-
-let arg1 = {"arg1": "Found 4 events in calendar 'family01962162451046263087@group.calendar.google.com':\n\n📅 ETA\n   🕒 2026-04-13T22:00:00Z - 2026-04-13T23:00:00Z\n\n📅 Baba embasa\n   🕒 2026-04-14T21:30:00Z - 2026-04-14T22:30:00Z\n\n📅 ETA\n   🕒 2026-04-17T10:00:00Z - 2026-04-17T11:00:00Z\n\n📅 ETA\n   🕒 2026-04-18T22:00:00Z - 2026-04-18T23:00:00Z\n\n"}
-
-console.log(main(arg1)) 
